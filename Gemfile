535a8a5cc22b07d3d1507b863e229558b8b97b03
Ubuntu <vaangadi@UbuntuOutsideLab1704.ajf3jfpbqciedfg4vg1bdudjea.gx.internal.cloudapp.net>
2017-11-23 00:19:39 +0000

    Adding tests based on test kitchen

---
+++ Gemfile
@@ -0,0 +1,6 @@
+ruby '2.4.2'
+
+source 'https://rubygems.org/' do
+  gem 'test-kitchen', '~>1.16'
+  gem 'kitchen-terraform' , '~>2.1'
+end
---
+++ test/integration/fixtures/variables.tf
@@ -0,0 +1,6 @@
+variable "location" {}
+variable "ssh_key" {}
+variable "resource_group_name" {
+    default = "terraform-test"
+}
+variable "vm_os_simple" {}
