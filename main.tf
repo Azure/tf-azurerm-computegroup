186071d37dfb218ab5c208a88e1fc0905e83ce5b
David Tesar <dtzar@hotmail.com>
2018-01-25 15:13:13 -0800

    fix lint

--- main.tf
+++ main.tf
@@ -80,6 +80,7 @@ resource "azurerm_virtual_machine_scale_set" "vm-linux" {
     publisher            = "Microsoft.Azure.Extensions"
     type                 = "CustomScript"
     type_handler_version = "2.0"
+
     settings = <<SETTINGS
     {
         "script": "${base64encode("${var.cmd_extension}")}"
